7d3f5432ae4ecda631cb9ac87ded1d33da1b81da
Brandon Rodriguez <brancowcanada@gmail.com>
2020-07-21 13:43:21 -0500

    se subio el build

---
+++ docs/precache-manifest.441f319899ca823be39564eb23239df1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ce876d6697af509e8f5206991858e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "353ecb601ebe6e51955b",
+    "url": "/static/css/2.485d0daa.chunk.css"
+  },
+  {
+    "revision": "716c5368ff3546ef8340",
+    "url": "/static/css/main.72ccea3f.chunk.css"
+  },
+  {
+    "revision": "353ecb601ebe6e51955b",
+    "url": "/static/js/2.715a0b6b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.715a0b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "716c5368ff3546ef8340",
+    "url": "/static/js/main.815ce0f2.chunk.js"
+  },
+  {
+    "revision": "19d519d64b4bfe8c9c46",
+    "url": "/static/js/runtime-main.8a882627.js"
+  },
+  {
+    "revision": "33613dbf9590f8ac298e55dacfb0f526",
+    "url": "/static/media/fondo-homepage.33613dbf.jpg"
+  }
+]);
